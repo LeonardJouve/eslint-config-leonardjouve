f06f9a1a9a5d85989cd442714c3d283ee2e4433b
Léonard Jouve <leonard.jouve@gmail.com>
2023-10-10 19:26:43 +0200

    allow inline config

--- index.js
+++ index.js
@@ -10,7 +10,6 @@ module.exports = {
     "plugins": [
         "@typescript-eslint",
     ],
-    "noInlineConfig": true,
     "reportUnusedDisableDirectives": true,
     "rules": {
         "arrow-body-style": ["error", "as-needed"],
